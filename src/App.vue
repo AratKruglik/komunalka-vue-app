b6031c10ee02066df6b55ff116dbd55589f1fd63
Oleksii Kruhlyk <oleksii.kruhlyk@gmail.com>
2025-07-07 11:26:45 +0300

    Implement project branding and login page structure.
    - Added project favicon and updated app title.
    - Developed `LoginPage.vue` with form, logo, and social login options.
    - Created reusable UI components: button, checkbox, and logo variations.
    - Updated styles with primary color palette.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
+<script setup>
+const version = 'v1.0.0'
+</script>
+
 <template>
   <main>
     <RouterView />
+    <div class="mt-8 text-center text-xs text-gray-500">
+      Build {{ version }}
+    </div>
   </main>
 </template>
---
+++ src/components/UI/forms/Checkbox.vue
@@ -0,0 +1,34 @@
+<script setup>
+defineProps({
+  checked: {
+    type: Boolean,
+    default: false
+  },
+  disabled: {
+    type: Boolean,
+    default: false
+  },
+  name: {
+    type: String
+  },
+  value: {
+    type: String
+  },
+  id: {
+    type: String
+  }
+})
+</script>
+
+<template>
+  <div class="flex h-6 shrink-0 items-center">
+    <div class="group grid size-4 grid-cols-1">
+      <input :id="id" :name="name" type="checkbox" class="h-4 w-4 accent-primary-500 rounded border-gray-300 text-primary-500 focus:ring-primary-500">
+    </div>
+  </div>
+  <label :for="name" class="block text-sm/6 text-gray-900">{{ value }}</label>
+</template>
+
+<style scoped>
+
+</style>
