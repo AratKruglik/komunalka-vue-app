68f8cb7416530714c9596423a18c3b33682f59bd
Oleksii Kruhlyk <oleksii.kruhlyk@gmail.com>
2025-08-06 14:30:55 +0300

    Add `AddUtilityPage` and `RegisterPage`; update router configuration
    - Introduced `AddUtilityPage.vue` for managing new utility meters, including form fields for address, type, and initial settings.
    - Added `RegisterPage.vue` for user registration with input validation and password strength indicators.
    - Updated `router.js` to include routes for adding utilities and user registration.

--- src/router/router.js
+++ src/router/router.js
@@ -1,12 +1,16 @@
 import {createWebHistory, createRouter} from 'vue-router'
 
 import LoginPage from '../components/pages/LoginPage.vue'
+import RegisterPage from '../components/pages/RegisterPage.vue'
+import AddUtilityPage from '../components/pages/AddUtilityPage.vue'
 import HomePage from "../components/pages/HomePage/HomePage.vue";
 import PersonalCabinetPage from "../components/pages/PersonalCabinetPage.vue";
 
 const routes = [
     {path: '/', component: HomePage},
     {path: '/login', component: LoginPage},
+    {path: '/register', component: RegisterPage},
+    {path: '/add-utility', component: AddUtilityPage},
     {path: '/personal-cabinet', component: PersonalCabinetPage},
 ]
 
